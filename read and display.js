3789629d2fbd2ed6af5fa9dc48ad8fdbb9a88052
Musa-The-Best <musaaleghari15@gmail.com>
2026-03-31 13:44:33 +0500

    Add files via upload

---
+++ read and display.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var fs = require('fs');
+fs.readFile('content.txt', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+}).listen(8080);
